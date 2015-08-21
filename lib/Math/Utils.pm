02c1da51a81cf6a3a103cb90f79170860b45b5eb
John M. Gamble <jgamble@cpan.org>
2015-08-20 19:16:04 -0500

    Added coefficient-list functions, minus pl_mult().

--- lib/Math/Utils.pm
+++ lib/Math/Utils.pm
@@ -11,12 +11,14 @@ our %EXPORT_TAGS = (
 	fortran => [ qw(log10 copysign) ],
 	compare => [ qw(generate_fltcmp generate_relational) ],
 	utility => [ qw(sign) ],
+	polynomial => [ qw(horner pl_add pl_sub pl_div) ],
 );
 
 our @EXPORT_OK = (
 	@{ $EXPORT_TAGS{fortran} },
 	@{ $EXPORT_TAGS{compare} },
 	@{ $EXPORT_TAGS{utility} },
+	@{ $EXPORT_TAGS{polynomial} },
 );
 
 our $VERSION = '0.02';
@@ -229,6 +231,216 @@ sub generate_relational
 		sub {return &$fltcmp(@_) <  0;},	# lt
 		sub {return &$fltcmp(@_) <= 0;},	# le
 	);
+}
+
+
+=head2 polynomial tag
+
+Perform some polynomial operations on plain lists of coefficients.
+
+The coefficient lists are presumed to go from low order to high, e.g.:
+
+    1 + 2x + 4x**2 + 8x**3
+
+becomes
+
+    (1, 2, 4, 8)
+
+In all functions the coeffcient list is passed by reference to the function.
+
+It is assumed that any leading zeros in the coefficient lists have
+already been removed before calling these functions, and that any leading
+zeros found in the returned lists will be handled by the caller.
+
+=head3 horner()
+
+    $y = horner(\@coefficients, $x);
+    @yvalues = horner(\@coefficients, \@xvalues);
+   
+Returns either a y-value for a corresponding x-value, or a list of
+y-values on the polynomial for a corresponding list of x-points,
+using Horner's method.
+
+=cut
+
+sub horner
+{
+	my @coefficients = @{$_[0]};
+	my $xval_ref = $_[1];
+
+	my @values;
+
+	#
+	# It could happen. Someone might type \$x instead of $x.
+	#
+	@values = (ref $xval_ref eq "ARRAY")? @$xval_ref:
+		(((ref $xval_ref eq "SCALAR")? $$xval_ref: $xval_ref));
+
+	#
+	# Move the leading coefficient off the polynomial list
+	# and use it as our starting value(s).
+	#
+	my @results = (pop @coefficients) x scalar @values;
+
+	foreach my $c (reverse @coefficients)
+	{
+		foreach my $j (0..$#values)
+		{
+			$results[$j] = $results[$j] * $values[$j] + $c;
+		}
+	}
+
+	return wantarray? @results: $results[0];
+}
+
+=head3 pl_add()
+
+    $polyn_ref = pl_add(\@m, \@n);
+   
+Add two lists of numbers as though they were polynomial coefficients.
+
+=cut
+
+sub pl_add
+{
+	my(@av) = @{$_[0]};
+	my(@bv) = @{$_[1]};
+	my $ldiff = scalar @av - scalar @bv;
+
+	my @result = ($ldiff < 0)?
+		splice(@bv, scalar @bv + $ldiff, -$ldiff):
+		splice(@av, scalar @av - $ldiff, $ldiff);
+
+	unshift @result, map($av[$_]+$bv[$_], 0..scalar @av);
+
+	return \@result;
+}
+
+=head3 pl_sub()
+
+
+    $polyn_ref = pl_sub(\@m, \@n);
+    
+Subtract the second list of numbers from the first as though they were polynomial coefficients.
+
+=cut
+
+sub pl_sub
+{
+	my(@av) = @{$_[0]};
+	my(@bv) = @{$_[1]};
+	my $ldiff = scalar @av - scalar @bv;
+
+	my @result = ($ldiff < 0)?
+		splice(@bv, scalar @bv + $ldiff, -$ldiff):
+		splice(@av, scalar @av - $ldiff, $ldiff);
+
+	unshift @result, map($av[$_]-$bv[$_], 0..scalar @av);
+
+	return \@result;
+}
+
+=head3 pl_div()
+
+    ($q_ref, $r_ref) = pl_div(\@coefficients1, \@coefficients2);
+ 
+Synthetic division for polynomials. Divides the first list of coefficients
+by the second list.
+
+Returns references to the quotient and the remainder.
+   
+=cut
+
+sub pl_div
+{
+	my @numerator = @{$_[0]};
+	my @divisor = @{$_[1]};
+
+	my @quotient;
+
+	my $n_degree = $#numerator;
+	my $d_degree = $#divisor;
+	my $q_degree = $n_degree - $d_degree;
+
+	#
+	# Sanity check if either set of coefficients
+	# are empty lists.
+	#
+	return ([0], \@numerator) if ($q_degree < 0);
+	return (undef, undef) if ($d_degree < 0);
+
+	my $lead_coefficient = $divisor[0];
+
+	#
+	# Perform the synthetic division. The remainder will
+	# be what's left in the numerator.
+	#
+	for my $j (reverse 0..$q_degree)
+	{
+		#
+		# Get the next term for the quotient. We shift
+		# off the lead numerator term, which would become
+		# zero due to subtraction anyway.
+		#
+		my $q = (pop @numerator)/$lead_coefficient;
+
+		push @quotient, $q;
+
+		for my $k (1..$d_degree)
+		{
+			$numerator[$k - 1] -= $q * $divisor[$k];
+		}
+	}
+
+	return (\@quotient, \@numerator);
+}
+
+=head3 pl_derivative()
+
+    $poly_ref = pl_derivative(\@coefficients)
+
+Returns the derivative of a polynomial.
+
+=cut
+
+sub poly_derivative
+{
+	my @coefficients = @_;
+	my $degree = $#coefficients;
+
+	return [] if ($degree <= 1);
+
+	$coefficients[$_] *= $_ for (2..$degree);
+
+	shift @coefficients;
+	return \@coefficients;
+}
+
+=head3 pl_antiderivative()
+
+    $poly_ref = pl_antiderivative(\@coefficients)
+
+Returns the antiderivative of a polynomial. The constant value is
+always set to zero and will need to be changed by the caller if a
+different constant is needed.
+
+=cut
+
+sub pl_antiderivative
+{
+	my @coefficients = @{$_[0]};
+	my $degree = scalar @coefficients;
+	my $n = 1;
+
+	#
+	# Sanity check if its an empty list.
+	#
+	return [0] if ($degree < 1);
+
+	$coefficients[$_] /= ++$n for (1..$degree - 1);
+
+	unshift @coefficients, 0;
+	return \@coefficients;
 }
 
 =head1 AUTHOR
